bdce271abadfb43afc3b1681b20569dee27c0f1a
orcunturkol <56793041+orcunturkol@users.noreply.github.com>
2020-05-28 20:22:03 +0300

    Proje Başlangıcı
    AppConfig kısmına ConnectionString tanımlandı.

---
+++ E-Ticaret/E-Ticaret/AnaMenu.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace E_Ticaret
+{
+    partial class AnaMenu
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnaMenu));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fiyat2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fiyat3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fiyat1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.userBox = new System.Windows.Forms.PictureBox();
+            this.sepetBox = new System.Windows.Forms.PictureBox();
+            this.kategorilerGroupBox = new System.Windows.Forms.GroupBox();
+            this.modaLabel = new System.Windows.Forms.LinkLabel();
+            this.elektronikLabel = new System.Windows.Forms.LinkLabel();
+            this.yasamLabel = new System.Windows.Forms.LinkLabel();
+            this.sporLabel = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sepetimLabel = new System.Windows.Forms.Label();
+            this.araLabel = new System.Windows.Forms.Label();
+            this.araBox = new System.Windows.Forms.TextBox();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sepetBox)).BeginInit();
+            this.kategorilerGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.fiyat2);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(471, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(400, 325);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(223, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "₺";
+            // 
+            // fiyat2
+            // 
+            this.fiyat2.AutoSize = true;
+            this.fiyat2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.fiyat2.Location = new System.Drawing.Point(176, 288);
+            this.fiyat2.Name = "fiyat2";
+            this.fiyat2.Size = new System.Drawing.Size(41, 24);
+            this.fiyat2.TabIndex = 9;
+            this.fiyat2.Text = "fiyat";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(39, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.fiyat3);
+            this.groupBox3.Controls.Add(this.pictureBox3);
+            this.groupBox3.Location = new System.Drawing.Point(934, 205);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(400, 325);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(222, 288);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "₺";
+            // 
+            // fiyat3
+            // 
+            this.fiyat3.AutoSize = true;
+            this.fiyat3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.fiyat3.Location = new System.Drawing.Point(175, 288);
+            this.fiyat3.Name = "fiyat3";
+            this.fiyat3.Size = new System.Drawing.Size(41, 24);
+            this.fiyat3.TabIndex = 9;
+            this.fiyat3.Text = "fiyat";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(36, 39);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.fiyat1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 205);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 325);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(204, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "₺";
+            // 
+            // fiyat1
+            // 
+            this.fiyat1.AutoSize = true;
+            this.fiyat1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.fiyat1.Location = new System.Drawing.Point(157, 288);
+            this.fiyat1.Name = "fiyat1";
+            this.fiyat1.Size = new System.Drawing.Size(41, 24);
+            this.fiyat1.TabIndex = 9;
+            this.fiyat1.Text = "fiyat";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // userBox
+            // 
+            this.userBox.Image = ((System.Drawing.Image)(resources.GetObject("userBox.Image")));
+            this.userBox.Location = new System.Drawing.Point(1163, 51);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(56, 52);
+            this.userBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.userBox.TabIndex = 17;
+            this.userBox.TabStop = false;
+            // 
+            // sepetBox
+            // 
+            this.sepetBox.Image = ((System.Drawing.Image)(resources.GetObject("sepetBox.Image")));
+            this.sepetBox.Location = new System.Drawing.Point(1060, 51);
+            this.sepetBox.Name = "sepetBox";
+            this.sepetBox.Size = new System.Drawing.Size(56, 52);
+            this.sepetBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sepetBox.TabIndex = 16;
+            this.sepetBox.TabStop = false;
+            // 
+            // kategorilerGroupBox
+            // 
+            this.kategorilerGroupBox.Controls.Add(this.modaLabel);
+            this.kategorilerGroupBox.Controls.Add(this.elektronikLabel);
+            this.kategorilerGroupBox.Controls.Add(this.yasamLabel);
+            this.kategorilerGroupBox.Controls.Add(this.sporLabel);
+            this.kategorilerGroupBox.Location = new System.Drawing.Point(12, 109);
+            this.kategorilerGroupBox.Name = "kategorilerGroupBox";
+            this.kategorilerGroupBox.Size = new System.Drawing.Size(1347, 61);
+            this.kategorilerGroupBox.TabIndex = 15;
+            this.kategorilerGroupBox.TabStop = false;
+            this.kategorilerGroupBox.Text = "Kategoriler";
+            // 
+            // modaLabel
+            // 
+            this.modaLabel.AutoSize = true;
+            this.modaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.modaLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.modaLabel.LinkColor = System.Drawing.Color.Black;
+            this.modaLabel.Location = new System.Drawing.Point(439, 16);
+            this.modaLabel.Name = "modaLabel";
+            this.modaLabel.Size = new System.Drawing.Size(74, 29);
+            this.modaLabel.TabIndex = 1;
+            this.modaLabel.TabStop = true;
+            this.modaLabel.Text = "Moda";
+            // 
+            // elektronikLabel
+            // 
+            this.elektronikLabel.AutoSize = true;
+            this.elektronikLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.elektronikLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.elektronikLabel.LinkColor = System.Drawing.Color.Black;
+            this.elektronikLabel.Location = new System.Drawing.Point(261, 16);
+            this.elektronikLabel.Name = "elektronikLabel";
+            this.elektronikLabel.Size = new System.Drawing.Size(120, 29);
+            this.elektronikLabel.TabIndex = 0;
+            this.elektronikLabel.TabStop = true;
+            this.elektronikLabel.Text = "Elektronik";
+            // 
+            // yasamLabel
+            // 
+            this.yasamLabel.AutoSize = true;
+            this.yasamLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.yasamLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.yasamLabel.LinkColor = System.Drawing.Color.Black;
+            this.yasamLabel.Location = new System.Drawing.Point(571, 16);
+            this.yasamLabel.Name = "yasamLabel";
+            this.yasamLabel.Size = new System.Drawing.Size(285, 29);
+            this.yasamLabel.TabIndex = 2;
+            this.yasamLabel.TabStop = true;
+            this.yasamLabel.Text = "Ev, Yaşam, Kırtasiye, Ofis";
+            // 
+            // sporLabel
+            // 
+            this.sporLabel.AutoSize = true;
+            this.sporLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sporLabel.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.sporLabel.LinkColor = System.Drawing.Color.Black;
+            this.sporLabel.Location = new System.Drawing.Point(914, 16);
+            this.sporLabel.Name = "sporLabel";
+            this.sporLabel.Size = new System.Drawing.Size(165, 29);
+            this.sporLabel.TabIndex = 3;
+            this.sporLabel.TabStop = true;
+            this.sporLabel.Text = "Spor, Outdoor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(1145, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Hesabım";
+            // 
+            // sepetimLabel
+            // 
+            this.sepetimLabel.AutoSize = true;
+            this.sepetimLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sepetimLabel.Location = new System.Drawing.Point(1056, 23);
+            this.sepetimLabel.Name = "sepetimLabel";
+            this.sepetimLabel.Size = new System.Drawing.Size(64, 25);
+            this.sepetimLabel.TabIndex = 13;
+            this.sepetimLabel.Text = "Sepet";
+            // 
+            // araLabel
+            // 
+            this.araLabel.AutoSize = true;
+            this.araLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.araLabel.Location = new System.Drawing.Point(278, 63);
+            this.araLabel.Name = "araLabel";
+            this.araLabel.Size = new System.Drawing.Size(49, 25);
+            this.araLabel.TabIndex = 14;
+            this.araLabel.Text = "Ara:";
+            // 
+            // araBox
+            // 
+            this.araBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.araBox.Location = new System.Drawing.Point(345, 54);
+            this.araBox.Name = "araBox";
+            this.araBox.Size = new System.Drawing.Size(316, 38);
+            this.araBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 721);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.userBox);
+            this.Controls.Add(this.sepetBox);
+            this.Controls.Add(this.kategorilerGroupBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sepetimLabel);
+            this.Controls.Add(this.araLabel);
+            this.Controls.Add(this.araBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sepetBox)).EndInit();
+            this.kategorilerGroupBox.ResumeLayout(false);
+            this.kategorilerGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label fiyat2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label fiyat3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label fiyat1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox userBox;
+        private System.Windows.Forms.PictureBox sepetBox;
+        private System.Windows.Forms.GroupBox kategorilerGroupBox;
+        private System.Windows.Forms.LinkLabel modaLabel;
+        private System.Windows.Forms.LinkLabel elektronikLabel;
+        private System.Windows.Forms.LinkLabel yasamLabel;
+        private System.Windows.Forms.LinkLabel sporLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label sepetimLabel;
+        private System.Windows.Forms.Label araLabel;
+        private System.Windows.Forms.TextBox araBox;
+    }
+}
+
